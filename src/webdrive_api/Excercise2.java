306b4a13a83d695a5f99cf5907b47a5622624fcd
Hoàng Đình Xuân <47933239+hoangdinhxuan@users.noreply.github.com>
2019-03-27 11:26:27 +0700

    Update Excercise2.java

--- src/webdrive_api/Excercise2.java
+++ src/webdrive_api/Excercise2.java
@@ -19,7 +19,10 @@ public class Excercise2 {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
-
+//	Exercise 1: 
+//		Step 1: Vào trang: https://daominhdam.github.io/basic-form/
+//			Step 2: Kiểm tra textbox Password bị disable bằng lệnh assertFalse()
+//			Step 3: Kiểm tra place holder có phải là giá trị: “Textbox is disabled” hay không?
 	@Test
 	public void TC01_check() throws InterruptedException {
 		driver.get("https://daominhdam.github.io/basic-form/");
@@ -33,7 +36,14 @@ public class Excercise2 {
 
 	}
 
-///////////////////////////////////////////////////////////////////////////////////////
+//	Exercise 2: 
+//		Step 1: Vào trang: http://demo.guru99.com/test/radio.html
+//		Step 2: Kiểm tra Radio Option1 có được chọn hay chưa? 
+//		Nếu chưa được chọn -> thực hiện click để chọn
+//		Sau đó kiểm tra Option1 đã được chọn( lệnh assertTrue(element. isSelected());
+//		Step 3: Kiểm tra Checkbox Checkbox3 có được chọn hay chưa? 
+//		Nếu chưa được chọn -> thực hiện click để chọn
+//		Sau đó kiểm tra Checkbox3 đã được chọn( lệnh assertTrue(element. isSelected());
 	@Test
 	public void TC02_checkoption() throws InterruptedException {
 		driver.get("http://demo.guru99.com/test/radio.html");
@@ -53,9 +63,31 @@ public class Excercise2 {
 
 	}
 
-	///////////////////////////////////////////////////////////////////////////////
+	//	Exercise 3:
+//		Step 1: Vào trang: https://www.facebook.com/
+//		Step 2: Select giá trị của các combobox tương ứng:
+//		Ngày: 14
+//		Tháng: Tháng 2
+//		Năm: 2018
+	@Test
+	public void TC03_Select() throws InterruptedException {
+		driver.get("https://www.facebook.com/");
+		Select selectday = new Select(driver.findElement(By.xpath("//*[@id='day']")));
+		selectday.selectByVisibleText("14");
+		String selectdate = selectday.getFirstSelectedOption().getText();
+		Assert.assertEquals("14", selectdate);
+		Thread.sleep(2000);
+		Select selectmonth = new Select(driver.findElement(By.xpath("//*[@id='month']")));
+		selectmonth.selectByVisibleText("Tháng 2");
+		Thread.sleep(2000);
+		String selectmont = selectmonth.getFirstSelectedOption().getText();
+		Assert.assertEquals("Tháng 2", selectmont);
+		Select selectyear = new Select(driver.findElement(By.xpath("//*[@id='year']")));
+		selectyear.selectByVisibleText("2018");
+		Thread.sleep(2000);
 		String selectyer = selectyear.getFirstSelectedOption().getText();
 		Assert.assertEquals("2018", selectyer);
+	
 
 	}
 
